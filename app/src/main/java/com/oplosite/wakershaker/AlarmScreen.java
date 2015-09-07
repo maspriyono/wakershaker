27db92eabd25fb29b66c948820d79940bb352903
priyono <priyono.arif@divusi.com>
2015-09-07 20:20:23 +0700

    first commit: v1

---
+++ app/src/main/java/com/oplosite/wakershaker/AlarmScreen.java
@@ -0,0 +1,200 @@
+package com.oplosite.wakershaker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class AlarmScreen extends Activity implements SensorListener {
+	
+	public final String TAG = this.getClass().getSimpleName();
+	private InterstitialAd interstitial;
+	private WakeLock mWakeLock;
+	private MediaPlayer mPlayer;
+
+	private static final int WAKELOCK_TIMEOUT = 60 * 1000;
+
+	/* put this into your activity class */
+	private SensorManager mSensorManager;
+	private float mAccel; // acceleration apart from gravity
+	private float mAccelCurrent; // current acceleration including gravity
+	private float mAccelLast; // last acceleration including gravity
+	private int wakeupLevel = 0;
+
+	private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+		public void onSensorChanged(SensorEvent se) {
+			float x = se.values[0];
+			float y = se.values[1];
+			float z = se.values[2];
+			mAccelLast = mAccelCurrent;
+			mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+			float delta = mAccelCurrent - mAccelLast;
+			mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+
+			TextView wakeupLevelTxt = (TextView) findViewById(R.id.wakeup_level);
+
+			if (mAccel > 5) {
+				wakeupLevel++;
+
+				wakeupLevelTxt.setText(wakeupLevel + "%");
+			}
+
+			if (wakeupLevel < 20) {
+				wakeupLevelTxt.setTextColor(Color.RED);
+			} else if (wakeupLevel < 60) {
+				wakeupLevelTxt.setTextColor(Color.YELLOW);
+			} else if (wakeupLevel < 100) {
+				wakeupLevelTxt.setTextColor(Color.GREEN);
+			}
+
+			if (wakeupLevel > 100) {
+				mPlayer.stop();
+				finish();
+			}
+		}
+
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//Setup layout
+		this.setContentView(R.layout.activity_alarm_screen);
+
+		String name = getIntent().getStringExtra(AlarmManagerHelper.NAME);
+		int timeHour = getIntent().getIntExtra(AlarmManagerHelper.TIME_HOUR, 0);
+		int timeMinute = getIntent().getIntExtra(AlarmManagerHelper.TIME_MINUTE, 0);
+		String tone = getIntent().getStringExtra(AlarmManagerHelper.TONE);
+		
+		TextView tvName = (TextView) findViewById(R.id.alarm_screen_name);
+		tvName.setText(name);
+		
+		TextView tvTime = (TextView) findViewById(R.id.alarm_screen_time);
+		tvTime.setText(String.format("%02d : %02d", timeHour, timeMinute));
+
+		//Play alarm tone
+		mPlayer = new MediaPlayer();
+		try {
+			if (tone != null && !tone.equals("")) {
+				Uri toneUri = Uri.parse(tone);
+				if (toneUri != null) {
+					mPlayer.setDataSource(this, toneUri);
+					mPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+					mPlayer.setLooping(true);
+					mPlayer.prepare();
+					mPlayer.start();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//Ensure wakelock release
+		Runnable releaseWakelock = new Runnable() {
+
+			@Override
+			public void run() {
+				getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+				getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+				getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+				getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+				if (mWakeLock != null && mWakeLock.isHeld()) {
+					mWakeLock.release();
+				}
+			}
+		};
+
+		new Handler().postDelayed(releaseWakelock, WAKELOCK_TIMEOUT);
+
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+		mAccel = 0.00f;
+		mAccelCurrent = SensorManager.GRAVITY_EARTH;
+		mAccelLast = SensorManager.GRAVITY_EARTH;
+
+		// Ads
+		AdView mAdView = (AdView) findViewById(R.id.adView);
+		AdRequest adRequest = new AdRequest.Builder().build();
+		mAdView.loadAd(adRequest);
+
+		interstitial = new InterstitialAd(AlarmScreen.this);
+		interstitial.setAdUnitId(String.valueOf(R.string.interstitial_ad_unit_id));
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// Set the window to keep screen on
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+		// Acquire wakelock
+		PowerManager pm = (PowerManager) getApplicationContext().getSystemService(Context.POWER_SERVICE);
+		if (mWakeLock == null) {
+			mWakeLock = pm.newWakeLock((PowerManager.FULL_WAKE_LOCK | PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), TAG);
+		}
+
+		if (!mWakeLock.isHeld()) {
+			mWakeLock.acquire();
+			Log.i(TAG, "Wakelock aquired!!");
+		}
+
+		mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		if (mWakeLock != null && mWakeLock.isHeld()) {
+			mWakeLock.release();
+		}
+
+		mSensorManager.unregisterListener(mSensorListener);
+	}
+
+	@Override
+	public void onSensorChanged(int sensor, float[] values) {
+		float x = values[0];
+		float y = values[1];
+		float z = values[2];
+		mAccelLast = mAccelCurrent;
+		mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+		float delta = mAccelCurrent - mAccelLast;
+		mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+
+	}
+
+	@Override
+	public void onAccuracyChanged(int sensor, int accuracy) {
+
+	}
+}
